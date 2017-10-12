0a2b6026a94bcc1efc629e98bebe69787535af09
VERO <VERO@DESKTOP-BGOP6S0>
2017-10-12 15:53:55 -0500

    mi aporte es holo en reporte1.java

---
+++ src/javaapplication4/Operaciones.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication4;
+
+/**
+ *
+ * @author VERO
+ */
+public class Operaciones {
+        public void algortmio1(){
+        System.out.print("ALGORITMO 1 \\n");
+    }
+     public void algortmio2(){
+        System.out.print("ALGORITMO 2 \\n");
+    }
+     public void algortmio3(){
+        System.out.print("ALGORITMO 3\\n");
+    }
+     public void algortmio4(){
+        System.out.print("ALGORITMO 4\\n");
+    }
+   //  System.out.print("ALGORITMO 5\\n");
+     
+     //  System.out.print("ALGORITMO 5\\n");
+     //  System.out.print("ALGORITMO 5\\n");
+     //  System.out.print("ALGORITMO 5\\n");
+}
+
